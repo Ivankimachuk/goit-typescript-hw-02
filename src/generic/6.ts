fde45796080a8bcbc9f7c54095ae64c5431dc1f8
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2023-10-18 21:34:50 +0300

    HMK-18/10/23

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -14,8 +14,15 @@ type User = {
   password: string;
 }
 
-function createOrUpdateUser(initialValues: User) {
+function createOrUpdateUser(initialValues: Partial<User>): User {
   // Оновлення користувача
+  const defaultUser: User = {
+    surname: '',
+    name: '',
+    email: '',
+    password: '',
+}
+return { ...defaultUser, ...initialValues };
 }
 
 createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
